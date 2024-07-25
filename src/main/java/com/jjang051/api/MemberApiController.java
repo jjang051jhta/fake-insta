fadfde619df1d26f3d7f765a8ccac0cca98dafba
jjang051jhta <jjang051jhta@gmail.com>
2024-07-25 14:25:11 +0900

    init

--- src/main/java/com/jjang051/api/MemberApiController.java
+++ src/main/java/com/jjang051/api/MemberApiController.java
@@ -4,11 +4,14 @@ import com.jjang051.entity.Member;
 import com.jjang051.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+
 
 import java.util.HashMap;
 import java.util.Map;
@@ -21,19 +24,23 @@ public class MemberApiController {
     private final MemberService memberService;
 
     @PutMapping("/member/{userId}/profileImageUrl")
-    public Map<String,Object> profileImageUpdate(@PathVariable String userId,
-                                                 MultipartFile profileImageUrl) {
+    public ResponseEntity<Map<String,Object>> profileImageUpdate(
+            @PathVariable String userId,
+            MultipartFile profileImageUrl) {
 
         log.info("userId==={}",userId);
-
         Member memberInfo =
                 memberService.changeProfileImage(userId,profileImageUrl);
-
         Map<String,Object> resultMap = new HashMap<>();
         resultMap.put("isUpload","ok");
-        //resultMap.put("memberInfo",memberInfo)
-        return resultMap;
-
+        resultMap.put("memberInfo",memberInfo);
+        if(memberInfo!=null) {
+            return ResponseEntity.status(HttpStatus.OK).body(resultMap);
+            //return ResponseEntity.ok().build(); //200
+            //return ResponseEntity.ok(resultMap); //200
+        }
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(resultMap);
+        //json으로 바껴서 리턴  응답코드
     }
 
 
